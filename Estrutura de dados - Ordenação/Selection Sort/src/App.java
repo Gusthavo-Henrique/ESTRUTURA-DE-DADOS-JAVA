8b50b628f4546f154839722c49ba7a1d0c3392c5
Gusthavo-Henrique <125114800+Gusthavo-Henrique@users.noreply.github.com>
2023-05-17 20:13:10 -0300

    Update App.java

--- Estrutura de dados - Ordenação/Selection Sort/src/App.java
+++ Estrutura de dados - Ordenação/Selection Sort/src/App.java
@@ -1,5 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] vetor = new int[10];
+        for(int i = 0 ; i < vetor.length ; i++) {
+            vetor[i] = (int) (Math.random()*vetor.length);
+            System.out.println(vetor[i]);
+        }
+        int aux;
+        for(int i = 0 ; i <vetor.length; i++) {
+            for(int j = i +1 ; j < vetor.length ; j++) {
+                if(vetor[i] < vetor[j]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[j];
+                    vetor[j] = aux;
+                }
+            }
+        }
+        System.out.println("--------------ORDENAÇÃO SELECT-------------");
+        for(int i = 0 ; i < vetor.length ; i++) {
+            System.out.println(vetor[i]);
+        }
     }
 }
+
